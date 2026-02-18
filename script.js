2c303ea0ec965ff9679197f228cbb1a3a86811db
swinhtun23-sudo <swinhtun23@gmail.com>
2026-02-18 11:14:42 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('header a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
